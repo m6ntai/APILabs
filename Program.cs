65d3913a537afea81dbf97314f54a4a54f0c70d4
student <student@507-4>
2026-01-27 16:18:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http.Json;
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7043/");
+var data = new
+{
+    A = 10,
+    B = 2
+};
+var response = await client.PostAsJsonAsync("api/calculator/calculate", data);
+var result = await response.Content.ReadAsStringAsync();
+Console.WriteLine($"Результат: {result}");
