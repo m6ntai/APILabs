89cdafae3efdcf6016b8799458d21769a12e848b
student <student@507-4>
2026-01-27 16:17:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost("calculate")]
+        public double Calculate(CalcRequest request)
+        {
+            // Операция зависит от варианта:
+            // 1: сложение, 2: умножение, 3: деление, 4: возведение в степень
+            return request.A + request.B;
+        }
+    }
+}
---
+++ Models/CalcRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class CalcRequest
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+    }
+}
